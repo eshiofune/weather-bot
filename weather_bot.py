d7755fe7cbfd38223b1f6ba882d0b46319da9a0a
Evans Ehiorobo <ehioroboevans@gmail.com>
2021-01-16 16:40:24 +0100

    Added tutorial edits

--- weather_bot.py
+++ weather_bot.py
@@ -3,12 +3,21 @@ import requests
 
 nlp = spacy.load("en_core_web_md")
 
-def get_weather(city_name):
-  response = requests.get("https://api.openweathermap.org/data/2.5/weather?q=" +
-    city_name + "&APPID=API_KEY").json()
-  weather = response["weather"][0]["description"]
+api_key = "your_api_key"
 
-  return weather
+def get_weather(city_name):
+    api_url = "http://api.openweathermap.org/data/2.5/weather?q={}&appid={}".format(city_name, api_key)
+
+    response = requests.get(api_url)
+    response_dict = response.json()
+
+    weather = response_dict["weather"][0]["description"]
+
+    if response.status_code == 200:
+        return weather
+    else:
+        print('[!] HTTP {0} calling [{1}]'.format(response.status_code, api_url))
+        return None
 
 def chatbot(statement):
   weather = nlp("Current weather in a city")
@@ -20,13 +29,16 @@ def chatbot(statement):
       if ent.label_ == "GPE": # GeoPolitical Entity
         city = ent.text
         break
-    else:
-      return "You need to tell me a city to check."
-
+      else:
+        return "You need to tell me a city to check."
+      
     city_weather = get_weather(city)
-    return "In " + city + ", the current weather is: " + city_weather
+    if city_weather is not None:
+      return "In " + city + ", the current weather is: " + city_weather
+    else:
+      return "Something went wrong."
   else:
     return "Sorry I don't understand that. Please rephrase your statement."
 
-response = chatbot("What is the weather in Lagos today?")
+response = chatbot("Is it going to rain in Rome today?")
 print(response)
